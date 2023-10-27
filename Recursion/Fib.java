f2bf5ec6f54300edbfdd62a8d963769f6875b667
birajdarnikita12 <niksbirajdar12@gmail.com>
2023-10-27 10:56:12 +0530

    Fib code added

---
+++ Recursion/Fib.java
@@ -0,0 +1,12 @@
+public class Fib
+{
+	public static void main(String[] args) {
+		System.out.println(fib(7));
+	}
+	public static int fib(int n){
+	    if(n<2){
+		return n;
+	    }
+	    return fib(n-1)+fib(n-2);
+	}
+}
